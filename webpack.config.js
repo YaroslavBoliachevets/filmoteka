cac7aa4a9728e2b99b50890bca231b80343de7ec
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2024-12-14 11:17:36 +0000

    webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+	clean: true,
+  },
+  mode: 'development',
+
+  module: {
+    rules: [
+		{
+			test: /\.s[ac]ss$/i,
+			use: [
+			  // Creates `style` nodes from JS strings
+			  "style-loader",
+			  // Translates CSS into CommonJS
+			  "css-loader",
+			  // Compiles Sass to CSS
+			  "sass-loader",
+			],
+		  },
+
+		  {
+			test: /\.(png|jpg|jpeg|gif|svg)$/i,
+			type: 'asset/resource',
+		  },
+    ],
+  },
+
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+	hot: true, // Включает горячую замену модулей (HMR)
+    liveReload: true, // Автоматически перезагружает страницу при изменении
+    watchFiles: ['./src/**/*'], // Следит за файлами в папке src
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Укажите правильный путь к вашему index.html
+    }),
+  ],
+
+};
